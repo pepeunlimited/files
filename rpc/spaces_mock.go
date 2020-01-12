37c6b7caed95e3ba108de073794f4d61691c7554
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-01-12 23:40:12 +0200

    added mocks for SpacesMock

--- rpc/spaces_mock.go
+++ rpc/spaces_mock.go
@@ -7,11 +7,17 @@ import (
 
 type SpacesMock struct {
 	Errors 		errorz.Stack
+	IsFile      bool
+	File        *File
 }
 
 func (s *SpacesMock) GetFile(context.Context, *GetFileParams) (*File, error) {
+	s.IsFile = true
 	if s.Errors.IsEmpty() {
-		return &File{}, nil
+		if s.File == nil {
+			return &File{}, nil
+		}
+		return s.File, nil
 	}
 	return nil, s.Errors.Pop()
 }
