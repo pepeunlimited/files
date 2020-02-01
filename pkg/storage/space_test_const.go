783c13d3bae4c530f2b6f6facfb48fcf66573f06
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-02-01 15:10:37 +0200

    .gitignore is now working

--- pkg/storage/space_test_const.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-var Endpoint		   string = "fra1.digitaloceanspaces.com"
-var AccessKey 	  	   string = "5K6L62V4JC27JAXCARLK"
-var SecretKey          string = "KNJeXNwBleUQRNxbFGHm3DuwzoYs28czYA2jpQbuqw8"
-var BucketName         string = "test-bucket-667"//somemeaninglesscodename-prod
-var AccessToken        string = "91e2d43c344e7e649117e454dd0762e8e928167a2e6dacdd221deae1fb99e22d"
